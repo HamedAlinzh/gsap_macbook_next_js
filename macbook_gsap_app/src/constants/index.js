e19e5000a4cde845aa6bb72a52cb7a51564985e2
HamedAlinezhad <halinezhad325@gmail.com>
2025-11-06 01:49:54 +0330

    Adding 3D model section

--- macbook_gsap_app/src/constants/index.js
+++ macbook_gsap_app/src/constants/index.js
@@ -6,4 +6,28 @@
 //     { label: 'Vision'},
 //     { label: 'AirPods'},
 // ];
-export const navLinks = ['Store', 'Mac', 'iphone', 'Watch', 'Vision', 'AirPods']
+const navLinks = ['Store', 'Mac', 'iphone', 'Watch', 'Vision', 'AirPods']
+
+const noChangeParts = [
+    "Object_84",
+    "Object_37",
+    "Object_34",
+    "Object_12",
+    "Object_80",
+    "Object_35",
+    "Object_36",
+    "Object_13",
+    "Object_125",
+    "Object_76",
+    "Object_33",
+    "Object_42",
+    "Object_58",
+    "Object_52",
+    "Object_21",
+    "Object_10",
+];
+
+export {
+    navLinks,
+    noChangeParts,
+}
