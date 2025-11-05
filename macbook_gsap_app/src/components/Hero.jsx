a3f78e3f3b6e12c8e79ad3c87e3a06b4969e67cb
HamedAlinezhad <halinezhad325@gmail.com>
2025-11-05 19:21:57 +0330

    Adding Hero section

---
+++ macbook_gsap_app/src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+const Hero = () => {
+    // const videoRef = useRef();
+    // useEffect(() => {
+    //     if(videoRef.current) videoRef.current.playbackRate = 2;
+    // }, []);
+
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
+
+            <video src="/video/hero.mp4" autoPlay muted playsInline />
+
+            <button>Buy</button>
+            <p>From 200mil or 25mil/mo. for 12 months</p>
+        </section>
+    )
+}
+
+export default Hero
--- macbook_gsap_app/src/constants/index.js
+++ macbook_gsap_app/src/constants/index.js
@@ -1,8 +1,9 @@
-export const navLinks = [
-    {label: 'Store'},
-    {label: 'Mac'},
-    {label: 'iPhone'},
-    {label: 'Watch'},
-    {label: 'Vision'},
-    {label: 'AirPods'},
-]
+// export const navLinks = [
+//     { label: 'Store'},
+//     { label: 'Mac'},
+//     { label: 'iPhone'},
+//     { label: 'Watch'},
+//     { label: 'Vision'},
+//     { label: 'AirPods'},
+// ];
+export const navLinks = ['Store', 'Mac', 'iphone', 'Watch', 'Vision', 'AirPods']
