51f11ec026504c7d8a425200262a8ca36e96d5af
HamedAlinezhad <halinezhad325@gmail.com>
2025-11-07 01:01:26 +0330

    Adding performance component

---
+++ macbook_gsap_app/src/components/Features.jsx
@@ -0,0 +1,10 @@
+const Features = () => {
+  return (
+    <section id="features">
+      <h2>Key Features</h2>
+      {/* Your content here */}
+    </section>
+  );
+};
+
+export default Features;
